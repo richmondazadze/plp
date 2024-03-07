c1dc3e3ba15765c39b49686002d11b6466e10110
richmondazadze <richmondazadze1313@gmail.com>
2024-03-07 21:35:16 +0000

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+location = input("Enter your location: ")
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
